dbef30a1be0a7f6dee11f4778d726928a743219b
Iraj Imran <irajimran5@gmail.com>
2026-06-28 05:57:15 -0700

    Fixed the issue of API

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "https://thinkboard-backend-two.vercel.app/api"; 
+const BASE_URL = "https://thinkboard-backend.vercel.app"; 
 
 const api = axios.create({
     baseURL: BASE_URL,
